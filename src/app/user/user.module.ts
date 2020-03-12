71e150c1df66d9770d68498d2e8c323bc1b84e3d
Manu Arora <mannuarora700@gmail.com>
2020-03-12 18:59:12 +0530

    Custom login // signup // forgot password and google oAuth forms

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from "@angular/common";
 
 import { UserRoutingModule } from "./user-routing.module";
 import { SharedModule } from "../shared/shared.module";
-import { LoginPageComponent } from './login-page/login-page.component';
-import { GoogleSigninDirective } from './google-signin.directive';
+import { LoginPageComponent } from "./login-page/login-page.component";
+import { GoogleSigninDirective } from "./google-signin.directive";
+import { ReactiveFormsModule } from "@angular/forms";
+import { EmailLoginComponent } from './email-login/email-login.component';
 
 @NgModule({
-  declarations: [LoginPageComponent, GoogleSigninDirective],
-  imports: [CommonModule, UserRoutingModule, SharedModule]
+  declarations: [LoginPageComponent, GoogleSigninDirective, EmailLoginComponent],
+  imports: [CommonModule, UserRoutingModule, SharedModule, ReactiveFormsModule]
 })
 export class UserModule {}
